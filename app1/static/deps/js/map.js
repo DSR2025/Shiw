c64061f66720e2ebea8c834acee1aa89583f5dbb
dimka97 <dmitriisanaia@gmail.com>
2025-02-28 23:32:57 +0400

    add new 768

--- app1/static/deps/js/map.js
+++ app1/static/deps/js/map.js
@@ -16,6 +16,22 @@ function init() {
         iconImageOffset: [-20, -40]
     });
 
+    var myMap = new ymaps.Map("map_mobile", {
+        center: [56.313623, 44.028301],
+        zoom: 15
+    });
+
+    var myPlacemark = new ymaps.Placemark([56.313623, 44.028301], {
+        hintContent: 'SHIW'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: '../static/deps/img/main_page/map_icon.svg',
+        iconImageSize: [55, 55],
+        iconImageOffset: [-20, -40]
+    });
+
+
     myMap.geoObjects.add(myPlacemark);
 
 }
+
--- app1/static/deps/js/scroll.js
+++ app1/static/deps/js/scroll.js
@@ -1,108 +1,122 @@
+function initScrollReveal() {
+  if (window.innerWidth >= 768) {
+    ScrollReveal().reveal('.about_content_left_wrapper', {
+      delay: 300,
+      distance: '50px',
+      origin: 'left',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-ScrollReveal().reveal('.about_content_left_wrapper', {
-    delay: 300,
-    distance: '50px',
-    origin: 'left',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.about_content_right_wrapper', {
+      delay: 500,
+      distance: '50px',
+      origin: 'right',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.about_content_right_wrapper', {
-    delay: 500,
-    distance: '50px',
-    origin: 'right',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.catalog_preview_item', {
+      delay: 200,
+      distance: '50px',
+      origin: 'bottom',
+      interval: 200,
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.catalog_preview_item', {
-    delay: 200,
-    distance: '50px',
-    origin: 'bottom',
-    interval: 200,
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.individual_title_wrapper', {
+      delay: 300,
+      distance: '50px',
+      origin: 'top',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.individual_title_wrapper', {
-    delay: 300,
-    distance: '50px',
-    origin: 'top',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.individual_contacts_wrapper', {
+      delay: 500,
+      distance: '50px',
+      origin: 'bottom',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.individual_contacts_wrapper', {
-    delay: 500,
-    distance: '50px',
-    origin: 'bottom',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.sale_box_big_wrapper', {
+      delay: 300,
+      distance: '50px',
+      origin: 'left',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.sale_box_big_wrapper', {
-    delay: 300,
-    distance: '50px',
-    origin: 'left',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.sale_box_small_wrapper', {
+      delay: 500,
+      distance: '50px',
+      origin: 'right',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.sale_box_small_wrapper', {
-    delay: 500,
-    distance: '50px',
-    origin: 'right',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.present_title', {
+      delay: 300,
+      distance: '50px',
+      origin: 'top',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.present_title', {
-    delay: 300,
-    distance: '50px',
-    origin: 'top',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.present_btn_wrapper_main', {
+      delay: 500,
+      distance: '50px',
+      origin: 'bottom',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.present_btn_wrapper_main', {
-    delay: 500,
-    distance: '50px',
-    origin: 'bottom',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.contacts_left_wrapper', {
+      delay: 300,
+      distance: '50px',
+      origin: 'left',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.contacts_left_wrapper', {
-    delay: 300,
-    distance: '50px',
-    origin: 'left',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.contacts_right_wrapper', {
+      delay: 500,
+      distance: '50px',
+      origin: 'right',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
-  ScrollReveal().reveal('.contacts_right_wrapper', {
-    delay: 500,
-    distance: '50px',
-    origin: 'right',
-    easing: 'ease-in-out',
-    reset: true
-  });
+    ScrollReveal().reveal('.faq_title_question', {
+      delay: 300,
+      distance: '50px',
+      origin: 'top',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
+    ScrollReveal().reveal('.faq_download_link', {
+      delay: 500,
+      distance: '50px',
+      origin: 'bottom',
+      easing: 'ease-in-out',
+      reset: false
+    });
 
+    ScrollReveal().reveal('.about_content_left_wrapper', {
+      delay: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--animation-delay')),
+      distance: getComputedStyle(document.documentElement).getPropertyValue('--animation-distance'),
+      origin: 'left',
+      easing: 'ease-in-out',
+      reset: false
+    });
+  }
+}
 
-  ScrollReveal().reveal('.faq_title_question', {
-    delay: 300,
-    distance: '50px',
-    origin: 'top',
-    easing: 'ease-in-out',
-    reset: true
-  });
+// Инициализация ScrollReveal при загрузке страницы
+initScrollReveal();
 
-
-  ScrollReveal().reveal('.faq_download_link', {
-    delay: 500,
-    distance: '50px',
-    origin: 'bottom',
-    easing: 'ease-in-out',
-    reset: true
-  });
+// Обновление ScrollReveal при изменении размера окна
+window.addEventListener('resize', initScrollReveal);
